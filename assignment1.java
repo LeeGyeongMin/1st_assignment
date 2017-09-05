98044b5f29098ab1e98e41c51b3ab446a1841759
LeeGyeongMin <31638900+LeeGyeongMin@users.noreply.github.com>
2017-09-05 11:03:46 +0900

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,9 @@
+
+public class assignment1 {
+	public static void main(String[] args) {
+		System.out.println("Student num : 2016301053");
+		System.out.println("NAme : Lee Gyeong Min");
+		System.out.println("P number : 010 9142 6707");
+		System.out.println("GitHub Add : github.com/LeeGyeongMin");
+	}
+}
